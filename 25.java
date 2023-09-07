1ab59c33f3f12979e471c7a265fd47fc22dbcff9
ayanalala23 <143515824+ayanalala23@users.noreply.github.com>
2023-09-07 21:42:07 +0530

    Command line argument

---
+++ 25.java
@@ -0,0 +1,9 @@
+//Command line argument
+
+public class MyClass{
+    public static void main(String args[]){
+    for(int i=0;i<args.length;i++){
+        System.out.println(args[i]);
+    }
+    }
+}
